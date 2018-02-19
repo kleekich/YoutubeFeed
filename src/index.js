5a183e4509dcce517ffda686a094bb6eee675e6d
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-02-19 15:11:28 -0800

    components files and folder created

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import ReactDom from 'react-dom';
 
 // Create a new component. This component should produce some HTML
 //const for final variable(ES6 syntax)
-const App = function() {
+const App = () => {
 
 	//return JSX
 	return <div>Hi!</div>;
 }
-
-
-
 
 
 //This this compnent's generated and put it on the page(in the DOM)
