4878f8a270a0f2fcf71d5db231ca4767603218bd
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-02-19 14:52:32 -0800

    index.js updated to render component on our DOM

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+import React from 'react';
+
+
 // Create a new component. This component should produce some HTML
+//const for final variable(ES6 syntax)
+const App = function() {
+
+	//return JSX
+	return <div>Hi!</div>;
+}
+
+
+
+
+
+//This this compnent's generated and put it on the page(in the DOM)
+React.render(App);
+
+
+
