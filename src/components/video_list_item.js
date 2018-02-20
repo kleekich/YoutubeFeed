cf43680adaaf4bf2738b2730d38d924f2174304e
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-02-20 15:43:00 -0800

    our image tag and title of snippet successfully loaded to our list

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -6,16 +6,18 @@ const VideoListItem = ({video}) => {
 const VideoListItem = (props) => {}
 	const video = props.video;
 */
+	const imageUrl = video.snippet.thumbnails.default.url;
+
 	return (
 		<li className="list-group-item">
 			<div className="video-list media">
 				<div className ="media-left">
-					<img className="media-object" />
+					<img className="media-object" src={imageUrl}/>
 				</div>
 			</div>
 
 			<div className="media-body">
-				<div className="media-heading"></div>
+				<div className="media-heading">{video.snippet.title}</div>
 			</div>
 		</li>
 	);
