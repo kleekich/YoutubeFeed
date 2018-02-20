aa8c581066a9ad592da7ace3b8342a280838258a
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-02-19 22:47:16 -0800

    input's onChange attribute added

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,11 +5,13 @@ import React, {Component} from 'react';
 //ES6 class components
 class SearchBar extends Component {
 	render() {
-		return <input />;
+		return <input onChange={this.onInputChange} />;
+	}
+
+
+	onInputChange() {
+
 	}
 }
 
-
-
-
- export default SearchBar;
+export default SearchBar;
