64b9bf5adcfbdbaffb76dfb91cee282bca784d26
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-02-19 22:54:22 -0800

    onCHange attribute has changed to lambda function using ES6 instead of onInputChange method

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,14 +4,21 @@ import React, {Component} from 'react';
 
 //ES6 class components
 class SearchBar extends Component {
+	/* without ES6
 	render() {
 		return <input onChange={this.onInputChange} />;
 	}
 
-
-	onInputChange() {
-
+	onInputChange(event) {
+		console.log(event.target.value);
 	}
+	*/
+
+	//ES6 Syntax with lamda-like function
+	render() {
+		return <input onChange={(event) => console.log(event.target.value)} />;
+	}
+
 }
 
 export default SearchBar;
