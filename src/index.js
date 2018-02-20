99e7148495200bffaeed414812b14c403dbe2515
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-02-20 14:54:50 -0800

    YTSearch line moved into App constructor for initializing list, and edited with ES6 syntax

--- src/index.js
+++ src/index.js
@@ -6,13 +6,21 @@ import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyAh2klEtZe45QYuzOwlWUN4UVzPNxpMqOc';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-	console.log(data);
-});
+
 
 // Create a new component. This component should produce some HTML
 //const for final variable(ES6 syntax)
 class App extends Component  {
+	constructor(props){
+		super(props);
+
+		this.state = {videos: {}}
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, data => {
+		this.setState({viedos: data});
+
+		});
+	}
 	//return JSX
 	render() {
 		return (
