86652e011eaff0a6f3e9fa1156208a5e25bd8cff
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-02-20 16:28:52 -0800

    App's state adds selectedVideo for detail view

--- src/index.js
+++ src/index.js
@@ -16,7 +16,11 @@ class App extends Component  {
 	constructor(props){
 		super(props);
 
-		this.state = {videos: []}
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+		
 
 		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
 			this.setState({ videos});
@@ -29,7 +33,7 @@ class App extends Component  {
 		return (
 			<div>
 				<SearchBar />
-				<VideoDetail video={this.state.videos[0]} />
+				<VideoDetail video={this.state.selectedVideo} />
 				<VideoList videos={this.state.videos}	 />
 
 			</div>
