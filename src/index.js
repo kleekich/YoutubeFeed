9ac4f26e0f06c7f2d5346ca4807a510bd49d14b3
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2019-01-04 14:26:53 -0800

    Use different API key

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
 import VideoDetail from './components/video_detail'
 
-const API_KEY = 'AIzaSyAh2klEtZe45QYuzOwlWUN4UVzPNxpMqOc';
+const API_KEY = 'AIzaSyCpNGhgOqGsWhD502t0PBuS4GB8i5mIDJc';
 
 
 
